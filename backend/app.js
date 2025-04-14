40a08c317be848751eac4354e1deb698b34abe0b
Abhi Gupta <abhiguptaofficial30@gmail.com>
2025-04-14 22:02:01 +0530

    final commit

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,10 @@ connect();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://chat-application-with-ai.onrender.com",
+    credentials: true
+}));  
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
