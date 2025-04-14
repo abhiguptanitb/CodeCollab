0abdc8827d20f4785ad4b463faafc3fd71e5b617
Abhi Gupta <abhiguptaofficial30@gmail.com>
2025-04-14 17:48:25 +0530

    final commit

--- frontend/src/config/webContainer.js
+++ frontend/src/config/webContainer.js
@@ -5,7 +5,10 @@ let webContainerInstance = null;
 
 export const getWebContainer = async () => {
     if (webContainerInstance === null) {
+        console.log("Creating new WebContainer instance...");
         webContainerInstance = await WebContainer.boot();
+        console.log("WebContainer instance created");
     }
+    console.log("WebContainer instance created:");
     return webContainerInstance;
 }
