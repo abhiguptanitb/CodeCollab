e703feaa9fa8d2f9548b0b48f4d39a8266772978
Abhi Gupta <abhiguptaofficial30@gmail.com>
2025-05-19 13:51:05 +0530

    final commit

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,16 @@ import projectRoutes from './routes/project.routes.js';
 import aiRoutes from './routes/ai.routes.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+
 connect();
 
 const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+    next();
+});
 
 app.use(cors());
 app.use(morgan('dev'));
@@ -18,16 +25,10 @@ app.use(cookieParser());
 
 app.use('/users', userRoutes);
 app.use('/projects', projectRoutes);
-app.use("/ai", aiRoutes)
-
-app.use((req, res, next) => {
-    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-    next();
-});
+app.use('/ai', aiRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-export default app; 
+export default app;
